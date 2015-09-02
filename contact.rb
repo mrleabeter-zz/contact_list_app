4cb1bd4f8b2b138e49456453aee624abb18d49da
Christopher Leabeter <christopherleabeter@gmail.com>
2015-09-02 17:54:35 -0400

    Contact List App - Task 7 Created method to add multiple phone numbers to each contact.

--- contact.rb
+++ contact.rb
@@ -6,34 +6,30 @@ class Contact
     @first_name = first_name
     @last_name = last_name
     @email = email
+    @phone_number_hash = {}
+    @contact_array = []
+  end
+
+  def create
+    id = ContactDatabase.total_contacts
+    @contact_array << id +=1
+    @contact_array << @first_name
+    @contact_array << @last_name
+    @contact_array << @email
+  end
+
+  def add_phone_num(input)
+    (label, number) = input.split(",")
+    @phone_number_hash[label] = number
+    @contact_array << @phone_number_hash
+  end
+
+  def save
+    ContactDatabase.add_to_database(@contact_array)
   end
  
   ## Class Methods
   class << self
-    def create(first_name, last_name, email)
-      new_contact = Contact.new(first_name, last_name, email)
-      id = ContactDatabase.total_contacts
-      contact_array = []
-      contact_array << id +=1
-      contact_array << first_name
-      contact_array << last_name
-      contact_array << email
-      
-      ContactDatabase.add_to_database(contact_array)
-    end
-
-    def phone_number
-      phone_number_hash = {}
-      input = ""
-      label = ""
-      while input != "done" do
-        puts "Please enter a label and phone number using the following format: label, XXX-XXX-XXXX."
-        puts "Or enter 'done' to complete the new contact"
-        input = STDIN.gets.chomp
-        (label, number) = input.split(",")
-        phone_number_hash[label] = number
-      end
-    end
     
   end
  
